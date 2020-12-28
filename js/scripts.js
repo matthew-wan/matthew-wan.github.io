7ddb2bf48be21371bfb806c6eff0f303adc00583
Matt235789 <CWAN203@caledonian.ac.uk>
2020-12-28 21:53:28 +0000

    meta tags

--- js/scripts.js
+++ js/scripts.js
@@ -5,62 +5,28 @@
     */
     (function ($) {
     "use strict"; // Start of use strict
-    var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+    var app = document.getElementById('app');
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+    
+    typewriter.typeString('Welcome!')
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString('Bienvenu!')
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString('Herzlich Willkommen!')
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString('欢迎!')
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString('أهلا بك')
+        .pauseFor(2000)
+        .deleteAll()
+        .start();
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
